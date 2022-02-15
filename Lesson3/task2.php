2da18bde0a6ff5449508494364640e2e5a74bd14
v1kuznetsov <vladislav1kuznetsov@gmail.com>
2022-02-15 12:05:59 +0200

    add previous hw

---
+++ Lesson3/task2.php
@@ -0,0 +1,10 @@
+<?php
+  $num = rand(1,99);
+  echo "$num<br/>";
+  if ($num % 2 == 0) {
+    echo "Odd";
+  }
+  else {
+    echo "Even";
+  }
+?>
---
+++ Lesson3/task3.php
@@ -0,0 +1,38 @@
+<?php
+  $num = rand(1,51);
+  echo "$num <br/>";
+  switch ($num) {
+    case '1':
+      echo "Odd";
+      break;
+    case '2':
+      echo "Odd";
+      break;
+    case '3':
+      echo "Odd";
+      break;
+    case '4':
+      echo "Odd";
+      break;
+    case '5':
+      echo "Odd";
+      break;
+    case '10':
+      echo "Even";
+      break;
+    case '20':
+      echo "Even";
+      break;
+    case '30':
+      echo "Even";
+      break;
+    case '40':
+      echo "Even";
+      break;
+    case '50':
+      echo "Even";
+      break;
+    default:
+      echo "Ooops";
+  }
+?>
