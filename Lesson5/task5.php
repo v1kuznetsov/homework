9eba3130adad0749092d32f0d4f90795f7de241f
v1kuznetsov <vladislav1kuznetsov@gmail.com>
2022-02-18 22:59:31 +0200

    endTask5

--- Lesson5/task5.php
+++ Lesson5/task5.php
@@ -1,14 +1,28 @@
 <?php
   function bestPassword($pas) {
-    $upperLetter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U' ,'V' ,'W', 'X', 'Y', 'Z'];
-    $nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    $sChars = ['!', '@', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '#'];
-    // НЕ ПОЙМУ ПОЧЕМУ ФУНКЦИЯ in_array ПРИ СРАВНЕНИИ ДВУХ МАССИВОВ ВСЕГДА ВЫДАЕТ false
+     $upperLetter = ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U' ,'V' ,'W', 'X', 'Y', 'Z'];
+     $nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+     $sChars = ["!", "@", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "#"];
     $pas = str_split($pas);
-    $up = in_array($upperLetter, $pas);
-    $nu = in_array($num, $pas);
-    $sc = in_array($sChars, $pas);
-    var_dump ($upperLetter);
+    foreach ($upperLetter as $val) {
+      if (in_array($val, $pas)) {
+        $up = true;
+      }
+    }
+    foreach ($nums as $val) {
+      if (in_array($val, $pas)) {
+        $nu = true;
+      }
+    }
+    foreach ($sChars as $val) {
+      if (in_array($val, $pas)) {
+        $sc = true;
+      }
+    }
+    // $up = in_array($upperLetter, $pas);
+    // $nu = in_array($num, $pas);
+    // $sc = in_array($sChars, $pas);
+     var_dump ($nu);
 
     if (count($pas) >= 8 && $up == true && $nu == true && $sc == true) {
       echo 'Сложный пароль';
@@ -21,5 +35,9 @@
     }
   }
 
-  bestPassword('1234A5667');
+  bestPassword("1234AF!@78");
+
+  // $pas = ['A','a','A'];
+  // var_dump(in_array("A", $pas));
+
 ?>
