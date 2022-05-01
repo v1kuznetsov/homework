7bbb0ee73ac01ad26ba2fcf74a8fc2e5cfec8ddc
v1kuznetsov <vladislav1kuznetsov@gmail.com>
2022-05-02 01:01:32 +0300

    repair_Lesson6

--- Lesson6/task5.php
+++ Lesson6/task5.php
@@ -1,16 +1,35 @@
 <?php
   function findInArray ($arr, $char) {
+    global $res;
+    global $loop;
+    $loop = 0;
+
     foreach ($arr as $key => $val) {
-      if ($val === $char) {
-        return $key;
+    if ($loop >= 0){        
+      if (is_array($val)) {
+        $loop++;
+        $res [] = $key;
+        findInArray($val, $char);        
       }
       else {
-        if (is_array($val)) {
-          findInArray($val, $char);
+            if ($val === $char) {
+              $res[] = $key;
+              // if (count($res) > $loop += 1) {
+              //   for ($i = count($res); $i > $loop; $i--) {
+              //     unset($res[count($res) - 2]);
+              //   }
+              // }
+              $loop = -1;
+              var_dump($res);
+              return $res;
+            }
+          }
+        } 
+        else {
+          break;
         }
-      }
     }
-  }
+  } 
 
-  findInArray([ 1, [ 5, "B"], 2, 3 ], 5);
+  findInArray([ 1, [ [4,4,[4,5]], "B"], [1, 5], 4, 5], 5);
 ?>
