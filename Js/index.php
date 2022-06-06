b8f4402d6e4a17d280c2fe9b7d1a0c36d0ba897e
Vlad <vladislav1kuznetsov@gmail.com>
2022-06-06 19:34:11 +0300

    finalTwig

--- Js/index.php
+++ Js/index.php
@@ -7,5 +7,47 @@ use \Twig\Environment;
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
 
-echo $twig->render('index.html.twig');
+$form_array = array (
+    array (
+      'labelName' => 'id',
+      'name' => 'id',
+      'id' => 'id',
+      'placeholder' => 'int'
+    ),
+    array (
+      'labelName' => 'name',
+      'name' => 'name',
+      'id' => 'name',
+      'placeholder' => 'string'
+    ),
+    array (
+      'labelName' => 'price',
+      'name' => 'price',
+      'id' => 'price',
+      'placeholder' => 'int'
+    ),
+    array (
+      'labelName' => 'season',
+      'name' => 'season',
+      'id' => 'season',
+      'placeholder' => 'string'
+    )
+  );
+
+  $titles = array (
+    'history' => 'История',
+    'allTable' => 'Вся таблица',
+    'button' => 'Добавить продукт',
+    'store' => 'Склад'
+  );
+
+  $tables = array (
+    'name' => 'name',
+    'price' => 'price',
+    'season' => 'season',
+    'id' => 'id'
+  );
+
+
+echo $twig->render('index.html.twig', ['form_array' => $form_array, 'titles' => $titles, 'tables' => $tables]);
 ?>
